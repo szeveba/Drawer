20c3995e721c487d1caf87d4feb54ea2bc804ab7
szeveba <szeveba@gmail.com>
2024-09-26 09:40:49 +0200

    positioning works

--- ConsoleDrawer/Program.cs
+++ ConsoleDrawer/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //█▓▒░
+            char symbol = '█';
+            ConsoleKeyInfo info;
+            do
+            {
+                info = Console.ReadKey();
+                switch (info.Key)
+                {
+                    case ConsoleKey.UpArrow: if (0 < Console.CursorTop) Console.CursorTop -= 1; break;
+                    case ConsoleKey.DownArrow: if (Console.CursorTop < Console.WindowHeight - 1) Console.CursorTop += 1; break;
+                    case ConsoleKey.LeftArrow: if (0 < Console.CursorLeft) Console.CursorLeft -= 1; break;
+                    case ConsoleKey.RightArrow: if (Console.CursorLeft < Console.WindowWidth - 1) Console.CursorLeft += 1; break;
+                    case ConsoleKey.F1: symbol = '█'; break;
+                    case ConsoleKey.F2: symbol = '▓'; break;
+                    case ConsoleKey.F3: symbol = '▒'; break;
+                    case ConsoleKey.F4: symbol = '░'; break;
+                    case ConsoleKey.Spacebar:
+                        var cursorPosition = Console.GetCursorPosition();
+                        Console.Write(symbol);
+                        Console.SetCursorPosition(cursorPosition.Left, cursorPosition.Top);
+                        break;
+                }
+            } while (info.Key != ConsoleKey.Escape);
         }
     }
 }
