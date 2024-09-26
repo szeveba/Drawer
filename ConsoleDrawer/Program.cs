1f19d2b8a2e3229a88173eea2df956226565da99
szegedi.barnabas <szegedi.barnabas@BOT218-000>
2024-09-26 09:30:30 +0200

    Add project files.

---
+++ ConsoleDrawer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleDrawer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
